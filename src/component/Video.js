af4ee9f575d8d09ade4ab349a8e48257ae425ffa
Your Name <you@example.com>
2021-01-28 16:27:13 +0530

    modal implemented

--- src/component/Video.js
+++ src/component/Video.js
@@ -1,28 +1,54 @@
-import React from 'react'
+import React,{useState} from 'react'
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import CloseIcon from '@material-ui/icons/Close';
 
 import './video.scss'
-const video = () => {
+const Video = () => {
+    const [showOverlay, setShowOverlay] = useState(true);
+    const [open, setOpen] = useState(false)
 
     const playvideo = e => {
-        e.target.play()
+        // e.target.play()
+        setShowOverlay(true)
     }
 
     const pausevideo = e => {
-        e.target.pause()
+        // e.target.pause()
+        setShowOverlay(false)
     }
+
+    const openVideo = () => {
+        console.log("Video");
+        return (
+            <div className="videoModal">
+                <video src="https://htmlreference.io/assets/HTML%205%20Video.mp4" controls="true" />
+            </div>
+        )
+    }
+
+    console.log("OVerlay", open);
 
     return (
         
         <div class="wrapper">
+        {open && (
+                        <div className="videoModal">
+                            <video src="https://htmlreference.io/assets/HTML%205%20Video.mp4" controls="true" />
+                            <button onClick={() => {setOpen(false)}}><CloseIcon /></button>
+                        </div>
+                    )}
             
-            {/* <h1>Netflix</h1> */}
             <section id="section1">
                 <a href="#section3">&#60;</a>
-                <div class="item" > 
+                <div class="item"> 
                     {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYtsiJzlvKCPCDrMrJBCmaLhVY-HkNnvMYJg&usqp=CAU" alt="Movie" width="350" height="256.703" /> */}
-                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                    <div className={showOverlay ? 'overlay true' : 'overlay'}>
+                        {showOverlay && (<button onClick={() => setOpen(true)}><PlayCircleFilledIcon /></button>)}</div>
+                    
+                    <video preload="auto" loop width="350" height="256.703">
                         <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
                     </video>
+                    
                     
                 </div>
                 <div class="item" > 
@@ -82,4 +108,4 @@ const video = () => {
     )
 }
 
-export default video
+export default Video
