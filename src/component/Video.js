af75bba758a9e33331e2654e1d0bb32c24d0ab1c
amitquantavid <amit.kumar@quantavid.com>
2021-01-28 15:30:29 +0530

    set all video mp4

--- src/component/Video.js
+++ src/component/Video.js
@@ -19,61 +19,88 @@ const video = () => {
             <section id="section1">
                 <a href="#section3">&#60;</a>
                 <div class="item" > 
-                    {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYtsiJzlvKCPCDrMrJBCmaLhVY-HkNnvMYJg&usqp=CAU" alt="Movie" width="350" height="256.703" /> */}
                     <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
                         <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
                     </video>
                     
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThI7OWfmYOvGDMmrB7ypX8K-Rm2i9r2avglw&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9RCLT8RnHYS1NItHA7n8SkzRCdH59m6NUZQ&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThI7OWfmYOvGDMmrB7ypX8K-Rm2i9r2avglw&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxpUJarUSjrlf9pJyhpsfiaN0RK6e7xG7W6A&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <a href="#section2" >&#62;</a>
             </section>
             <section id="section2">
                 <a href="#section1" >&#60;</a>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThI7OWfmYOvGDMmrB7ypX8K-Rm2i9r2avglw&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxpUJarUSjrlf9pJyhpsfiaN0RK6e7xG7W6A&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpMVVfc4yKxzoKu_9iMbz41b7EbofGW10GwQ&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxpUJarUSjrlf9pJyhpsfiaN0RK6e7xG7W6A&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpMVVfc4yKxzoKu_9iMbz41b7EbofGW10GwQ&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <a href="#section3" >&#62;</a>
             </section>
             <section id="section3">
                 <a href="#section2" >&#60;</a>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYtsiJzlvKCPCDrMrJBCmaLhVY-HkNnvMYJg&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThI7OWfmYOvGDMmrB7ypX8K-Rm2i9r2avglw&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9RCLT8RnHYS1NItHA7n8SkzRCdH59m6NUZQ&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThI7OWfmYOvGDMmrB7ypX8K-Rm2i9r2avglw&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <div class="item" > 
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxpUJarUSjrlf9pJyhpsfiaN0RK6e7xG7W6A&usqp=CAU" alt="Movie" width="350" height="256.703" />
+                    <video preload="auto" loop onMouseOver={playvideo} onMouseOut={pausevideo} width="350" height="256.703">
+                        <source src="https://htmlreference.io/assets/HTML%205%20Video.mp4" type="video/mp4"></source>
+                    </video>                
                 </div>
                 <a href="#section1" >&#62;</a>
             </section>
